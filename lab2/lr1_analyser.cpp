c7c37cb3f62555d4476b0e46104c7ff2da22d4be
Charley <jiaxin_desu@163.com>
2021-10-09 22:38:11 +0800

    LR(1)语法分析器开了个头

--- lab2/lr1_analyser.cpp
+++ lab2/lr1_analyser.cpp
@@ -1,5 +1,30 @@
 #include "lr1_analyser.h"
 
 LR1Analyser::LR1Analyser() {
-    
+    at_init();
+    stack_init();
 }
+
+
+// 栈初始化
+void LR1Analyser::stack_init() {
+    // 先清空
+    while (!state_stack.empty()) {
+        state_stack.pop();
+    }
+    while (!symbol_stack.empty()) {
+        symbol_stack.pop();
+    }
+    // 初始化
+    state_stack.push(0);
+    char *temp = (char*)malloc(sizeof(char) * 2);
+    temp[0] = '#';
+    temp[1] = '\0';
+    symbol_stack.push(temp);
+}
+
+
+void LR1Analyser::at_init() {
+
+}
+
--- lab2/lr1_analyser.h
+++ lab2/lr1_analyser.h
@@ -1,9 +1,50 @@
 #ifndef __LR1_ANALYSER_H__
 #define __LR1_ANALYSER_H__
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+#define AT_COL_ACTION 0
+#define AT_COL_GOTO 1
+
+#define AT_TYPE_SHIFT 0
+#define AT_TYPE_REDUCE 1
+#define AT_TYPE_ACC 2
+#define AT_TYPE_GOTO 3
+
+typedef struct ATindex {
+    int col;
+    int state;
+    char *symbol;
+}ATindex;
+
+typedef struct ATval {
+    int type;
+    int val;
+}ATval;
+
 class LR1Analyser {
 public:
+    // 分析表
+    map<ATindex, ATval> analyse_table;
+
+    // 状态栈
+    stack<int> state_stack;
+
+    // 符号栈
+    stack<char*> symbol_stack;
+
     LR1Analyser();
+
+    // 栈初始化
+    void stack_init();
+
+    // 分析表初始化
+    void at_init();
 
 private:
 
